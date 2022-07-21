83a8a905eb2d490d9cee373150b044d3ddd6c154
DY-76 <9754745@naver.com>
2022-07-21 19:32:51 +0900

    AJAX, KAKAO MAP API , POPUP UPDATE

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const fs = require("fs");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express'});
+});
+
+/* map popup */
+router.post('/map', function(req, res, next) {
+  res.render('map', { title: 'Express'});
 });
 
 module.exports = router;
--- routes/server.js
+++ routes/server.js
@@ -2,11 +2,23 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
-/* POST */
+/* POST-CSV Download */
 router.post('/csv2server', function(req, res, next) {
   const data = req.body.data;
   console.log("On!")
   fs.writeFileSync('csv_data.csv', data);
+});
+
+/* TEST */
+router.post('/testPost', function(req, res, next) {
+  const data = req.body.data;
+  console.log("testPost_Success!")
+  res.send({result:"testPost_Success!"});
+});
+router.get('/testGet', function(req, res, next) {
+  const data = req.body.data;
+  console.log("testGet_Success!")
+  res.send({result:"testGet_Success!"});
 });
 
 module.exports = router;
